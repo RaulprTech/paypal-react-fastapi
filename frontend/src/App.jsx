0a58b7554d24a86f06f730425aeb36c692a86cd1
raulprtech <raul_vrm_2134@hotmail.com>
2022-03-26 00:00:45 -0600

    view upgrade

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import "./App.css";
 import PaypalButton from "./components/PaypalButton";
 
 function App() {
   return (
-    <div>
-      <h1>Hola</h1>
-      <PaypalButton/>
+    <div className="container">
+      <div className="card">
+        <header className="header">
+          <h1 className="title">Paga Aqui</h1>
+        </header>
+        <PaypalButton />
+      </div>
     </div>
   );
 }
